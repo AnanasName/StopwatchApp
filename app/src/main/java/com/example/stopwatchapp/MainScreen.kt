28be1cecbce0e8b9d70bf83656c562d0870a3673
AnanasName <martin25102002@gmail.com>
2022-10-07 14:08:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stopwatchapp/MainScreen.kt
@@ -0,0 +1,131 @@
+package com.example.stopwatchapp
+
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.TextStyle
+import com.example.stopwatchapp.service.ServiceHelper
+import com.example.stopwatchapp.service.StopwatchService
+import com.example.stopwatchapp.service.StopwatchState
+import com.example.stopwatchapp.ui.theme.Light
+import com.example.stopwatchapp.util.Constants.ACTION_SERVICE_CANCEL
+import com.example.stopwatchapp.util.Constants.ACTION_SERVICE_START
+import com.example.stopwatchapp.util.Constants.ACTION_SERVICE_STOP
+import android.hardware.lights.Light as Light1
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun MainScreen(stopwatchService: StopwatchService) {
+    val context = LocalContext.current
+    val hours by stopwatchService.hours
+    val minutes by stopwatchService.minutes
+    val seconds by stopwatchService.seconds
+    val currentState by stopwatchService.currentState
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.surface)
+            .padding(30.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier.weight(weight = 9f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AnimatedContent(targetState = hours, transitionSpec = { addAnimation() }) {
+                Text(
+                    text = hours,
+                    style = TextStyle(
+                        fontSize = MaterialTheme.typography.h1.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        color = if (hours == "00") White else Blue
+                    )
+                )
+            }
+            AnimatedContent(targetState = minutes, transitionSpec = { addAnimation() }) {
+                Text(
+                    text = minutes, style = TextStyle(
+                        fontSize = MaterialTheme.typography.h1.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        color = if (minutes == "00") White else Blue
+                    )
+                )
+            }
+            AnimatedContent(targetState = seconds, transitionSpec = { addAnimation() }) {
+                Text(
+                    text = seconds, style = TextStyle(
+                        fontSize = MaterialTheme.typography.h1.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        color = if (seconds == "00") White else Blue
+                    )
+                )
+            }
+        }
+        Row(modifier = Modifier.weight(weight = 1f)) {
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(0.8f),
+                onClick = {
+                    ServiceHelper.triggerForegroundService(
+                        context = context,
+                        action = if (currentState == StopwatchState.Started) ACTION_SERVICE_STOP
+                        else ACTION_SERVICE_START
+                    )
+                }, colors = ButtonDefaults.buttonColors(
+                    backgroundColor = if (currentState == StopwatchState.Started) Red else Blue,
+                    contentColor = White
+                )
+            ) {
+                Text(
+                    text = if (currentState == StopwatchState.Started) "Stop"
+                    else if ((currentState == StopwatchState.Stopped)) "Resume"
+                    else "Start"
+                )
+            }
+            Spacer(modifier = Modifier.width(30.dp))
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(0.8f),
+                onClick = {
+                    ServiceHelper.triggerForegroundService(
+                        context = context, action = ACTION_SERVICE_CANCEL
+                    )
+                },
+                enabled = seconds != "00" && currentState != StopwatchState.Started,
+                colors = ButtonDefaults.buttonColors(disabledBackgroundColor = Light)
+            ) {
+                Text(text = "Cancel")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+fun addAnimation(duration: Int = 600): ContentTransform{
+    return slideInVertically(animationSpec = tween(durationMillis = duration)){height -> height}  + fadeIn(
+        animationSpec = tween(durationMillis = duration)
+    ) with slideOutVertically(animationSpec = tween(durationMillis = duration)) {height -> height} + fadeOut(
+        animationSpec = tween(durationMillis = duration)
+    )
+}
